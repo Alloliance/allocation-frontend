a99155a321c8b4f77b8d322c6b66eaa17e3eeaa0
thomas779 <thomasehumphreys@gmail.com>
2024-01-09 15:28:13 -0300

    feat: connect wallet

--- src/pages/InformationPage.tsx
+++ src/pages/InformationPage.tsx
@@ -5,11 +5,16 @@ import { PageContainer } from "../components/PageContainer";
 import { ArrowRight } from "../components/icons/ArrowRight";
 import { InformationBox } from "../components/InformationBox";
 
+import { useAccount } from "wagmi";
+import { WalletButton } from "@rainbow-me/rainbowkit";
+
 type Props = {
   activePage: Page;
   onGoToProfile: () => void;
 };
+
 export const InformationPage = ({ activePage, onGoToProfile }: Props) => {
+  const account = useAccount();
   return (
     <PageContainer
       classes={
@@ -18,9 +23,21 @@ export const InformationPage = ({ activePage, onGoToProfile }: Props) => {
     >
       <header className="flex flex-col">
         <div className="flex justify-end text-white my-4">
-          <Button onClick={onGoToProfile} classes="flex items-center gap-2">
-            <span>Get Started</span> <ArrowRight size="small" />
-          </Button>
+          {account.isConnected ? (
+            <Button onClick={onGoToProfile} classes="flex items-center gap-2">
+              <span>Get Started</span> <ArrowRight size="small" />
+            </Button>
+          ) : (
+            <WalletButton.Custom wallet="metamask">
+              {({ connect }) => {
+                return (
+                  <Button onClick={connect} classes="flex items-center gap-2">
+                    Connect Wallet
+                  </Button>
+                );
+              }}
+            </WalletButton.Custom>
+          )}
         </div>
         <div className="flex justify-center mt-28 mb-28 sm:mb-44 ">
           <h1 className="text-6xl font-lato text-pink-400 text-shadow-neon animate-hover sm:text-7xl">
