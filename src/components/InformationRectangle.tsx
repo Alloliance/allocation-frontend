9e7cb552a96ef55a16af3bfac1c39881b9e99105
Rebecca Fransson <ymafransson@gmail.com>
2024-01-10 22:23:32 +0100

    Information page (#7)
    * added more information to the start page
    
    * information page done
    
    * removed svg

--- src/components/InformationRectangle.tsx
+++ src/components/InformationRectangle.tsx
@@ -1,11 +1,14 @@
 type Props = {
+  imageSrc: string;
   title: string;
   subTitle: string;
 };
-export const InformationRectangle = ({ title, subTitle }: Props) => (
-  <div className="flex flex-row mx-20 gap-10">
-    <div className="border w-32 h-32 m-6">img</div>
-    <div className="flex flex-col mt-10">
+export const InformationRectangle = ({ imageSrc, title, subTitle }: Props) => (
+  <div className="flex flex-row gap-10">
+    <div className="flex justify-center m-6 min-w-24">
+      <img src={imageSrc} width="100" />
+    </div>
+    <div className="flex flex-col justify-center">
       <h4 className="text-pink-400 text-shadow-neon mb-4 text-xl">{title}</h4>
       <span className="text-white">{subTitle}</span>
     </div>
