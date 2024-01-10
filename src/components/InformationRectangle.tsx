a22eb36e1f2157e5c41336f2528392324eb8ba27
Rebecca Fransson <ymafransson@gmail.com>
2024-01-10 23:00:07 +0100

    Footer (#8)
    * footer

--- src/components/InformationRectangle.tsx
+++ src/components/InformationRectangle.tsx
@@ -6,7 +6,7 @@ type Props = {
 export const InformationRectangle = ({ imageSrc, title, subTitle }: Props) => (
   <div className="flex flex-row gap-10">
     <div className="flex justify-center m-6 min-w-24">
-      <img src={imageSrc} width="100" />
+      <img src={imageSrc} width="100" style={{ filter: "grayscale(100%)" }} />
     </div>
     <div className="flex flex-col justify-center">
       <h4 className="text-pink-400 text-shadow-neon mb-4 text-xl">{title}</h4>
