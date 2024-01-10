4529d52762dd5c260f08042805d71e0085f143aa
Rebecca Fransson <ymafransson@gmail.com>
2024-01-10 23:38:52 +0100

    Moved footer into own comp

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+export const Footer = () => (
+  <div className="bg-purple-900 flex flex-row gap-10 px-20 py-8 items-end mt-20">
+    <h4 className="text-pink-400 text-shadow-neon text-xl">Created by</h4>
+    <a
+      className="text-white hover:text-pink-300"
+      href="https://github.com/RebeccaFransson"
+    >
+      RebeccaFransson
+    </a>
+    <a
+      className="text-white hover:text-pink-300"
+      href="https://github.com/thomas779"
+    >
+      thomas779
+    </a>
+    <a
+      className="text-white hover:text-pink-300"
+      href="https://github.com/lajosdeme"
+    >
+      lajosdeme
+    </a>
+  </div>
+);
--- src/pages/InformationPage.tsx
+++ src/pages/InformationPage.tsx
@@ -10,6 +10,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Loading } from "../components/icons/Loading";
 import { useState } from "react";
 import { InformationRectangle } from "../components/InformationRectangle";
+import { Footer } from "../components/Footer";
 
 type Props = {
   activePage: Page;
@@ -139,27 +140,7 @@ export const InformationPage = ({ activePage, onGoToProfile }: Props) => {
             subTitle="Embrace the future with Alloliance, already making waves on Arbitrum Sepolia with a deployed contract at 0x9BAD40cD7c4d8Fb9b66778B1eAc02B14552E8b41."
           />
         </div>
-        <div className="bg-purple-900 flex flex-row gap-10 px-20  py-8 items-end">
-          <h4 className="text-pink-400 text-shadow-neon text-xl">Created by</h4>
-          <a
-            className="text-white hover:text-pink-300"
-            href="https://github.com/RebeccaFransson"
-          >
-            RebeccaFransson
-          </a>
-          <a
-            className="text-white hover:text-pink-300"
-            href="https://github.com/thomas779"
-          >
-            thomas779
-          </a>
-          <a
-            className="text-white hover:text-pink-300"
-            href="https://github.com/lajosdeme"
-          >
-            lajosdeme
-          </a>
-        </div>
+        <Footer />
       </div>
     </PageContainer>
   );
