a3097a3a45de96c45c1cef54e7ca209fdc0ccab2
Rebecca Fransson <ymafransson@gmail.com>
2024-01-10 22:57:54 +0100

    footer

--- src/pages/InformationPage.tsx
+++ src/pages/InformationPage.tsx
@@ -134,6 +134,27 @@ export const InformationPage = ({ activePage, onGoToProfile }: Props) => {
             subTitle="Embrace the future with Alloliance, already making waves on Arbitrum Sepolia with a deployed contract at 0x9BAD40cD7c4d8Fb9b66778B1eAc02B14552E8b41."
           />
         </div>
+        <div className="bg-purple-900 flex flex-row gap-10 px-20  py-8 items-end">
+          <h4 className="text-pink-400 text-shadow-neon text-xl">Created by</h4>
+          <a
+            className="text-white hover:text-pink-300"
+            href="https://github.com/RebeccaFransson"
+          >
+            RebeccaFransson
+          </a>
+          <a
+            className="text-white hover:text-pink-300"
+            href="https://github.com/thomas779"
+          >
+            thomas779
+          </a>
+          <a
+            className="text-white hover:text-pink-300"
+            href="https://github.com/ajosdeme"
+          >
+            ajosdeme
+          </a>
+        </div>
       </div>
     </PageContainer>
   );
