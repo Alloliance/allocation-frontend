d218c2de35c48fba9633a22d2bf0ee2db2763963
Rebecca Fransson <ymafransson@gmail.com>
2024-01-10 22:20:51 +0100

    information page done

--- src/pages/InformationPage.tsx
+++ src/pages/InformationPage.tsx
@@ -70,46 +70,68 @@ export const InformationPage = ({ activePage, onGoToProfile }: Props) => {
             ALLOLIANCE
           </h1>
           <h2 className="text-white text-2xl">
-            Veriify yourself on the blockchain
+            Unlocking Web3 Grants Compliance
           </h2>
         </div>
       </header>
       <div className="flex flex-col gap-20">
         <div className="flex justify-center text-center flex-wrap gap-10">
           <InformationQuadrant
-            title="Connect a wallet"
-            subTitle="Our revolutionary web3 app simplifies your online experience."
+            title="Web3 compliance protocol"
+            subTitle="Explore the future of grant-seeking with our web3 protocol, ensuring seamless and secure grant applications."
             button={
               <NeonButton onClick={onGoToProfile}>Get started</NeonButton>
             }
           />
           <InformationQuadrant
-            title="Upload verification"
-            subTitle="Our revolutionary web3 app simplifies your online experience."
+            title="Allo Protocol Magic"
+            subTitle="Experience Allo Protocol transforming the KYC journey for users, ensuring a smooth and secure submission process."
             button={
               <NeonButton onClick={onGoToProfile}>Get started</NeonButton>
             }
           />
           <InformationQuadrant
-            title="Discover all the apps"
-            subTitle="Our revolutionary web3 app simplifies your online experience."
+            title="Elevate Your Experience"
+            subTitle="Enjoy a streamlined KYC journey with easy document and selfie submission, paving the path for swift grant approval."
             button={
               <NeonButton onClick={onGoToProfile}>Get started</NeonButton>
             }
           />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col mx-20 gap-12">
           <InformationRectangle
-            title="Our revolutionary web3 app simplifies your online experience."
-            subTitle="Our revolutionary web3 app simplifies your online experience."
+            imageSrc="https://www.svgrepo.com/show/1320/rocket.svg"
+            title="Roadmap to KYC Success: Approvals and Beyond"
+            subTitle="Explore the roadmap features that make Alloliance a dynamic KYC platform, from third-party integrations to swift approvals, enhancing the grant-seeking process."
           />
           <InformationRectangle
-            title="Our revolutionary web3 app simplifies your online experience."
-            subTitle="Our revolutionary web3 app simplifies your online experience."
+            imageSrc="https://www.svgrepo.com/show/107865/building.svg"
+            title="Allo Profile Metadata Standard: Shaping the Future of Profiles"
+            subTitle="Delve into the Allo Profile Metadata Standard, a groundbreaking initiative that standardizes metadata, allowing organizations to issue credentials, achievements, and KYC details seamlessly."
           />
           <InformationRectangle
-            title="Our revolutionary web3 app simplifies your online experience."
-            subTitle="Our revolutionary web3 app simplifies your online experience."
+            imageSrc="https://www.svgrepo.com/show/776/dna.svg"
+            title="Connect the Dots: Allo Profile Metadata in Action"
+            subTitle="Learn how Allo Profile Metadata brings profiles to life, offering a standardized way to query, categorize, and reason about the rich array of metadata stored in Allo profiles."
+          />
+          <div className="my-4 flex flex-col items-center">
+            <h1 className="text-2xl font-lato text-pink-400 text-shadow-neon text-center">
+              ALLOLIANCE
+            </h1>
+            <div
+              className="bg-pink-800 w-96 h-8 relative bottom-8"
+              style={{ filter: "blur(50px)" }}
+            ></div>
+          </div>
+          <InformationRectangle
+            imageSrc="https://www.svgrepo.com/show/2878/certificate.svg"
+            title="AlloProfileMetadataIssuerRegistry.sol: A Foundation of Trust"
+            subTitle="Explore the core of Alloliance's metadata infrastructure with AlloProfileMetadataIssuerRegistry.sol, ensuring a secure and verifiable way to manage metadata for Allo profiles."
+          />
+          <InformationRectangle
+            imageSrc="https://www.svgrepo.com/show/1321/planet.svg"
+            title="Ready for the Future: Alloliance on Arbitrum Sepolia"
+            subTitle="Embrace the future with Alloliance, already making waves on Arbitrum Sepolia with a deployed contract at 0x9BAD40cD7c4d8Fb9b66778B1eAc02B14552E8b41."
           />
         </div>
       </div>
